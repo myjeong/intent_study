c1676ef75460c72a6893d7353568cd05b5aee7ef
myjeong <jeongm39@gmail.com>
2021-06-30 13:59:28 +0900

    04-4 플래그와 부가데이터 사용하기
    플래그에대한 이해와 Parcelable 인터페이스에대해 공부하기

--- app/src/main/java/com/example/test_0628/MainActivity.java
+++ app/src/main/java/com/example/test_0628/MainActivity.java
@@ -1,16 +1,15 @@
 package com.example.test_0628;
 
-import android.content.ComponentName;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int REQUEST_CODE_MENU=101;
+    public static final String KEY_SIMPLE_DATA="data";
 
 
     @Override
@@ -20,40 +19,22 @@ public class MainActivity extends AppCompatActivity {
         //뷰 객체 참조(이 레이아웃을 실행하겠다)
         setContentView(R.layout.activity_main);
 
-        //1. 인텐트에 액션과 데이터를 넣어 다른 앱의 액티비티를 띄움
-        final EditText editText = findViewById(R.id.editText);
         Button button=findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //입력상자에 입력된 전화번호 확인
-                String data = editText.getText().toString();
+                Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+                //정의한 SimpleData 클래스로 객체 생성
+                SimpleData data=new SimpleData(100,"Hello, Android");
 
-                //전화걸기 화면을 보여줄 인텐트 객체 생성(액션, 데이터)
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(data));
-                //액티비티 띄우기(응답받는 기능까지 하려면 startActivityForResult)
-                startActivity(intent);
+                //생성된 SimpleData 객체를 인텐트에 value로써 삽입
+                intent.putExtra(KEY_SIMPLE_DATA,data);
+
+                startActivityForResult(intent,REQUEST_CODE_MENU);
             }
         });
 
-        //2. 컴포넌트 이름을 이용해 새로운 액티비티를 띄움
-        Button button2=findViewById(R.id.button2);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //인텐트 객체 생성
-                Intent intent=new Intent();
-                // 컴포넌트 이름을 지정할 수 있는 객체 생성(컴포넌트 이름 이용)
-                ComponentName name=new ComponentName("org.techtown.samplecallintent",
-                        "org.techtown.samplecallintent.MenuActivity");
 
-                //인텐트 객체에 컴포넌트 지정
-                intent.setComponent(name);
-
-                //액티비티띄우기
-                startActivityForResult(intent, 101);
-            }
-        });
 
     }
 
